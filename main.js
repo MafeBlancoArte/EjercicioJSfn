d0b6e4a8c015e16bb4c28a201c21133059095949
MafeBlancoArte <blancodelarosamaria@gmail.com>
2024-01-08 12:07:26 +0100

    object literal edad

--- main.js
+++ main.js
@@ -111,12 +111,51 @@ const prices = [32, 14, 68, 54, 120, 87, 120, 90,];
     };
     console.log(prices);
 
-
-
+//5/ arrays/ Escribe una función que tome dos arrays como argumentos y devuelva un nuevo array con todos los elementos de ambos arrays
+const plats = [];
+   plats [0]= "Vegano";
+   plats [1]= "Extra Vegano";
+   plats [2]= "Ingles";
+   plats [3]= "Americano";
    
+const postres = [];
+    postres [0]= "Gluten Free";
+    postres [1]= "Lacto Free";
+    postres [2]= "Fructose Free";
+    postres [3]= "Cake day";
 
-         
+const menuA = plats.concat(postres);
+    console.log(menuA);
+// OBJETOS LITERALES 
+// 1.	Escribe una función que tome un objeto literal con una propiedad "nombre" y devuelva el valor de esa propiedad.
+
+const person1 = {
+    firstName: "Marcos",
+    lastName: "Gomez",
+    id: 3456,
+    fullName : function (){
+        return this.firstName + " " + this.lastName;
+    }
+};
+  console.log(person1.fullName());      
+
+// función que tome un objeto literal con una propiedad "edad" y un número como argumentos, y actualice el valor de la propiedad "edad" con el número dado.
+const person2 = {
+    firstName: "Teresa",
+    lastName: "Arteta",
+    id: 3457,
+    age: 65,
+    fullName2: function (){
+        return this.firstName + " " + this.lastName + " is " + this.age + " years old"; 
+    }
+};
+console.log(person2.fullName2());
+
+//una función que tome un objeto literal y 
+//una cadena de texto como argumentos, y 
+//agregue una nueva propiedad al objeto 
+//con la cadena de texto como nombre y un valor inicial de null.
+
+const person3 = {
     
-    
-        
-    
+}
